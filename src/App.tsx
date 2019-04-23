2bd7ede2b20f65bf594c4ac0b363ecd539f2f256
Omar Delarosa <thedelarosa@gmail.com>
2019-04-23 19:18:18 -0400

    tweaking css, adding link

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const APP_TITLE = 'lens';
+const AUTHOR_URL = 'https://omardelarosa.com';
+
+interface IAppState {
+    isExpanded?: boolean;
+}
+
+interface IAppProps {}
+
+class App extends Component<IAppState, IAppProps> {
+    public readonly state = {
+        isExpanded: false,
+    };
+
+    public toggleHeader = (e: React.SyntheticEvent) => {
+        this.setState({
+            isExpanded: !this.state.isExpanded,
+        });
+    };
+
     render() {
+        const isExpanded = this.state.isExpanded;
+        const headerClassName = `header flexy-container ${
+            isExpanded ? 'expanded' : 'collapsed'
+        }`;
+        const toggleButtonText = '...'; // This could be better?
         return (
-            <div className="App">
-                <h1>Lens</h1>
+            <div className="App" onClick={this.toggleHeader}>
+                <h1 className={headerClassName}>
+                    <span />
+                    <span className="titleText">{APP_TITLE}</span>
+                </h1>
+                {!isExpanded ? null : (
+                    <h2 className="info-box flexy-container">
+                        <span />
+                        <span>
+                            by{' '}
+                            <a href={AUTHOR_URL} target="_blank">
+                                omar delarosa
+                            </a>
+                        </span>
+                    </h2>
+                )}
             </div>
         );
     }
